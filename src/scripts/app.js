be5d79c88e872688f238d1dcd94792c02e0f56d4
thibault Poucet <thibault.poucet@gmail.com>
2023-05-14 17:02:31 +0200

    Ajout des police et navigation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,4 +4,40 @@ import { gsap } from "gsap";
 
 
 
-gsap.to("H1", {duration: 2, x: 200} );
+//gsap.to("H1", {duration: 2, x: 200} );
+
+
+//Dark Mode
+
+const darkTheme = document.querySelector(".darkmode");
+
+//Gérer le data-theme du body
+darkTheme.addEventListener("click", function(){
+    if(document.body.dataset.theme === "dark"){
+        light();
+        localStorage.setItem("theme", "light");
+    } else {
+        dark();
+        localStorage.setItem("theme", "dark");
+    } 
+});
+
+//Est ce que l"utilisateur veut un theme dark?
+const userDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+//Est ce que l'utilisateur a déjà indiqué une préférence sur notre site?
+let theme = localStorage.getItem('theme');
+if((!theme && userDark) || (theme === "dark")){
+    dark();
+} else if(theme === "light"){
+    light();
+}
+
+//function pour le dark
+function dark(){
+    document.body.setAttribute("data-theme", "dark");
+}
+//function pour le light
+function light(){
+    document.body.setAttribute("data-theme", "light");
+}
